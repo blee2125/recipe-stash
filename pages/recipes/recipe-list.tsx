2d1cae9af7d9e93d7ca41e9a1d8dfbfc18ecd125
Brandon Lee <blee2125@gmail.com>
2022-12-27 09:46:10 -0700

    recipe link is listed on new line

--- pages/recipes/recipe-list.tsx
+++ pages/recipes/recipe-list.tsx
@@ -2,38 +2,37 @@ import Link from 'next/link';
 import React, { useEffect } from 'react';
 
 export default function RecipeList() {
-    const [recipes, setRecipes] = React.useState<any[]>([])
+  const [recipes, setRecipes] = React.useState<any[]>([])
 
-    const getRecipes = () => {
-        fetch('/api/recipe', {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          })
-          .then((res) => res.json())
-          .then(res => setRecipes(res))
-    }
+  const getRecipes = () => {
+    fetch('/api/recipe', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((res) => res.json())
+    .then(res => setRecipes(res))
+  }
 
-    const mapRecipes = () => {
-        const recipesArray = recipes.map(recipe => {
-            return (<Link key={recipe._id} href={`${recipe._id}`}>{recipe.name}</Link>)
-        })
-        return recipesArray
-    }
+  const mapRecipes = () => {
+    const recipesArray = recipes.map(recipe => {
+        return (<div key={recipe._id} ><Link href={`${recipe._id}`}>{recipe.name}</Link></div>)
+    })
+    return recipesArray
+  }
 
-    useEffect(() => {
-        getRecipes();
-    }, []);
+  useEffect(() => {
+      getRecipes();
+  }, []);
 
-    return (
-        <>
-          <h1>recipe list</h1>
-          {/* <button onClick={getRecipes}>get</button> */}
-          {recipes.length > 0 ? mapRecipes() : 'no recipes'}
-          <h2>
-            <Link href="/">Back to home</Link>
-          </h2>
-        </>
-    );
+  return (
+    <>
+      <h1>Recipe list</h1>
+      {recipes.length > 0 ? mapRecipes() : 'no recipes'}
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
 }
