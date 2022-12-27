17f38c9eeda58c26efe2c3a760e9fe71fb383910
Brandon Lee <blee2125@gmail.com>
2022-12-27 09:46:38 -0700

    displays recipe data in table

--- pages/recipes/[pid].tsx
+++ pages/recipes/[pid].tsx
@@ -1,37 +1,73 @@
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react';
+import Link from 'next/link';
 
 const Recipe = () => {
-    const router = useRouter()
-    const { pid } = router.query
-    const [recipe, setRecipe] = React.useState()
+  const router = useRouter()
+  const { pid } = router.query
+  const [recipe, setRecipe] = React.useState({
+    name: '',
+    directions: [],
+    ingredients: []
+  })
 
-    const getRecipe = () => {
-    fetch(`/api/recipe?id=${pid}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  const getRecipe = () => {
+  fetch(`/api/recipe?id=${pid}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((res) => res.json())
+    .then(res => {
+      const recipe1 = res.filter((r: any) => r._id === pid)
+      setRecipe(recipe1[0])
+    })
+  }
+
+  const mapDir = () => {
+    if (recipe !== undefined) {
+      const dirArr = recipe.directions.map(dir => {
+        return <tr key={recipe.directions.indexOf(dir)+1}><td>{recipe.directions.indexOf(dir)+1}</td><td>{dir}</td></tr>
       })
-      .then((res) => res.json())
-      .then(res => {
-        const recipe1 = res.filter((r: any) => r._id === pid)
-        console.log(pid)
-        setRecipe(recipe1[0])
-      })
+      return dirArr
     }
+  }
 
-    useEffect(() => {
-        if (pid !== undefined) {
-           getRecipe(); 
-        }
-        
-    }, [pid]);
+  const mapIng = () => {
+    if (recipe !== undefined) {
+      const dirArr = recipe.ingredients.map(ing => {
+        return <tr key={recipe.ingredients.indexOf(ing)+1}><td>{ing[0]}</td><td>{ing[1]}</td><td>{ing[2]}</td></tr>
+      })
+      return dirArr
+    }
+  }
+
+  useEffect(() => {
+    if (pid !== undefined) {
+      getRecipe(); 
+    }
+  }, [pid]);
 
   return (
     <>
-        <p>id {pid}</p>
-        <p>{recipe !== undefined ? recipe['name'] : ''}</p>
+      <table>
+        <tbody>
+          <tr>
+            <td><b>Name</b></td>
+            <td>{recipe !== undefined ? recipe['name'] : ''}</td>
+          </tr>
+          <tr>
+            <td><b>Ingredients</b></td>
+          </tr>
+          {mapIng()}
+          <tr>
+            <td><b>Directions</b></td>
+          </tr>
+          {mapDir()}
+        </tbody>
+      </table>
+      <Link href="/recipes/recipe-list">Back to recipes</Link>
     </>
   )
 }
